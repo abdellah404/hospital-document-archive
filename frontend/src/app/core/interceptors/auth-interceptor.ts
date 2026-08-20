c9be07df4659067e205c5f38b061d26c9f99c0c1
abdellah404 <1234abdellah007@gmail.com>
2026-08-20 19:28:22 +0000

    feat: add token expire validation

--- frontend/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -1,17 +1,32 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from '../services/auth';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('access_token');
+ export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+    const token = authService.getToken();
 
-  if (!token) {
-    return next(req);
-  }
+    if (!token) {
+      return next(req);
+    }
 
-  const authReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  return next(authReq);
-};
+    return next(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          authService.logout();
+          void router.navigate(['/login']);
+        }
+
+        return throwError(() => error);
+      })
+    );
+  };
