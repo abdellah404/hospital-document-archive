97b6ac5ca948523c99e2e9a36a01865b4030dc08
abdellah404 <1234abdellah007@gmail.com>
2026-08-20 05:04:20 +0000

    docker setup for cloudflare tunnel

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:4200'],
+    allow_origins=[ os.getenv(
+            "FRONTEND_URL",
+            "http://localhost:4200",
+        )],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
